1c358f1228fa3420409c0ffb4b5527559c113f94
Anthony Shalagin <anthony.shalagin@gmail.com>
2016-07-31 18:18:21 -0400

    Added external storage access in manifest and wrote functionality to retrieve images from the phone

--- app/src/main/java/com/anthonyshalagin/photoimportdemo/MainActivity.java
+++ app/src/main/java/com/anthonyshalagin/photoimportdemo/MainActivity.java
@@ -1,13 +1,54 @@
 package com.anthonyshalagin.photoimportdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+
+import static android.R.attr.data;
+
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+		startActivityForResult(i, 1);
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		super.onActivityResult(requestCode, resultCode, data); //Called whenever we go to an external activity and then come back to our app
+
+		if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+
+			Uri selectedImage = data.getData();
+
+			try {
+
+				Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+
+				ImageView imageView = (ImageView) findViewById(R.id.imageView);
+				imageView.setImageBitmap(bitmapImage);
+
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+
+		}
+
 	}
 }
